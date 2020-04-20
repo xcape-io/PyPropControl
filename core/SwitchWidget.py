689959ec6d8fd33e9907cbb7b74e1669b5f2c817
fauresystems <34378695+fauresystems@users.noreply.github.com>
2020-04-20 17:54:41 +0200

    Create SwitchWidget.py

---
+++ core/SwitchWidget.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+SwitchWidget.py
+MIT License (c) Marie Faure <dev at faure dot systems>
+
+Prop switch widget.
+"""
+
+from PyQt5.QtWidgets import QWidget, QLabel, QHBoxLayout, QSizePolicy
+from PyQt5.QtCore import Qt, pyqtSlot, QSize
+from PyQt5.QtGui import QIcon
+
+class SwitchWidget(QWidget):
+
+    # __________________________________________________________________
+    def __init__(self, label, variable, options={}, label_width=0):
+        super(SwitchWidget, self).__init__()
+
+        self._variable = variable
+        self._image_on = None
+        self._image_off = None
+        self._image = False
+        self._value_on = '1'
+        self._value_off = '0'
+
+        self._dataLabel = QLabel(label + ' : ')
+        self._dataLabel.setAlignment(Qt.AlignLeft | Qt.AlignVCenter)
+        self._dataLabel.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        if label_width:
+            self._dataLabel.setFixedWidth(label_width)
+
+        self._dataValue = QLabel()
+        self._dataValue.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+
+        main_layout = QHBoxLayout()
+        main_layout.setContentsMargins(0, 0, 0, 0)
+        main_layout.setSpacing(8)
+        main_layout.addWidget(self._dataLabel)
+        main_layout.addWidget(self._dataValue)
+
+        self.setLayout(main_layout)
+
+        if 'image_on' in options and 'image_off' in options:
+            self._image_on = QIcon(options['image_on'])
+            self._image_off = QIcon(options['image_off'])
+            self._image = True
+            self._dataValue.setPixmap(self._image_off.pixmap(QSize(20, 20)))
+
+    # __________________________________________________________________
+    @pyqtSlot(dict)
+    def onDataReceived(self, variables):
+
+        if self._image:
+            if variables[self._variable] == self._value_on:
+                self._dataValue.setPixmap(self._image_on.pixmap(QSize(20, 20)))
+            else:
+                self._dataValue.setPixmap(self._image_off.pixmap(QSize(20, 20)))
+        elif self._variable in variables:
+            self._dataValue.setText(variables[self._variable])
